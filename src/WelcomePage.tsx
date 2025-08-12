89def8d534b145ef369753f93b48efa04b32734b
mustafashaheen1 <mustafashaheen61@gmail.com>
2025-08-12 14:50:31 -0400

    Updated WelcomePage.tsx

--- src/WelcomePage.tsx
+++ src/WelcomePage.tsx
@@ -218,7 +218,8 @@ export default function WelcomePage({ onNavigateToLogin, onSignUpSuccess }: Welc
                     <path d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z"/>
                   </svg>
                 </button>
-      </div>
+              </div>
+            </div>
 
             <div className="form-group">
               <label htmlFor="confirmPassword">Confirm Password</label>
@@ -242,7 +243,6 @@ export default function WelcomePage({ onNavigateToLogin, onSignUpSuccess }: Welc
                   </svg>
                 </button>
               </div>
-            </div>
             </div>
 
             <div className="form-group checkbox-group">
@@ -274,6 +274,4 @@ export default function WelcomePage({ onNavigateToLogin, onSignUpSuccess }: Welc
       </div>
     </div>
   );
-}
-  )
 }
